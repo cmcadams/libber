a6de2372f3828bd5ed83aa715fc9e09ea64a8a73
cmcadams <charliemcadams07@gmail.com>
2026-04-10 04:32:50 +0400

    fix supabase env setup

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
 
-
 const supabase = createClient(
-  'https://flghcbrwqtburdywgcvk.supabase.co',
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsZ2hjYnJ3cXRidXJkeXdnY3ZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2MDI1MjcsImV4cCI6MjA5MTE3ODUyN30.psbCQaOkCJiVefoK7tKCGW1dsv0gzVUbs-fOdRKztVY'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 )
-
 
 async function load() {
   const { data, error } = await supabase
